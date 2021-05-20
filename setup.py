6facb60385a5b2a0360bdb4b8b47ab41af1c5369
Lam Jun Rong <junron1@outlook.com>
2021-05-20 21:22:45 +0800

    Patch

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ctflib',
-      version='0.3.1',
+      version='0.3.2',
       description='[PRIVATE] Tools for speeding up CTFing',
       author='jro',
       install_requires=["z3-solver", "aiohttp", "requests", "beautifulsoup4", "pwntools"],
