92aaa81599aa2faf42f1c23ce439edfe5a73256c
Lam Jun Rong <junron1@outlook.com>
2022-06-12 10:51:59 +0800

    Add docker command

--- ctflib/scripts/main.py
+++ ctflib/scripts/main.py
@@ -1,3 +1,7 @@
+import os.path
+import shutil
+from pathlib import Path
+
 import click
 from pwnlib.elf import ELF
 
@@ -20,6 +24,12 @@ def cli(ctx):
 @click.option('--remote', '-r', default='', help='remote connection string')
 def pwn(remote: str):
     tg.generate_template(remote)
+    
+@cli.command()
+def docker():
+    shutil.copyfile(os.path.join(Path(__file__).parent, "dockerfiles", "bullseye-2.31"), "./bullseye-2.31")
+    shutil.copyfile(os.path.join(Path(__file__).parent, "dockerfiles", "docker-compose.yml"), "docker-compose.yml")
+
     
 @cli.command()
 @click.option('--elf', '-e', default='', help='Binary to patch')
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ctflib',
-      version='4.0.2',
+      version='4.1.0',
       description='[PRIVATE] Tools for speeding up CTFing',
       author='jro',
       install_requires=["z3-solver", "aiohttp", "requests", "pwntools", "click"],
