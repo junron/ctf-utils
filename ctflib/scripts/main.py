0149e6fd44db3b26637bf5be104cfe211f93bf23
Lam Jun Rong <junron1@outlook.com>
2022-06-12 11:43:07 +0800

    Fix docker command again

--- ctflib/scripts/main.py
+++ ctflib/scripts/main.py
@@ -27,8 +27,9 @@ def pwn(remote: str):
     
 @cli.command()
 def docker():
-    shutil.copyfile(os.path.join(Path(__file__).parent.parent, "pwn", "dockerfiles", "bullseye-2.31"), "./bullseye-2.31")
+    shutil.copyfile(os.path.join(Path(__file__).parent.parent, "pwn", "dockerfiles", "bullseye-2.31"), "./Dockerfile")
     shutil.copyfile(os.path.join(Path(__file__).parent.parent, "pwn", "dockerfiles", "docker-compose.yml"), "docker-compose.yml")
+    print("Run `sudo docker-compose run --rm vuln bash` to start container")
 
     
 @cli.command()
