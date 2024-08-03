73ab663304617574952deb064075cef20397e769
junron <junron1@outlook.com>
2024-08-03 15:55:34 +0800

    Update script name

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='ctflib',
-      version='4.7.0',
+      version='4.7.1',
       description='Tools for speeding up CTFing',
       author='jro',
       install_requires=["z3-solver", "aiohttp", "requests", "pwntools", "click"],
       entry_points={
             'console_scripts': [
-                  'ctf = ctflib.scripts.main:cli',
+                  'ctflib = ctflib.scripts.main:cli',
             ],
       },
       package_data={'': ['pwn/dockerfiles/bullseye-2.31','pwn/dockerfiles/docker-compose.yml']},
